d9bfbcf80fbd5c8a3213aa82d99bf18bca1088b0
kounelios13 <pkounelios@gmail.com>
2016-12-10 19:57:11 +0200

    updated code

--- src/com/demo/DemoApp.java
+++ src/com/demo/DemoApp.java
@@ -5,17 +5,21 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
+import com.AppUtils.core.XString;
 import com.AppUtils.gui.ApplicationScreen;
 import com.messages.Message;
 
 public class DemoApp extends ApplicationScreen {
 
 
-	String[] actionsArray = new String[]{"Show an info message","Show an info message with custom title"};
+	private XString codeToParse = new XString();
+	private String[] actionsArray = new String[]{"Show an info message","Show an info message with custom title"};
 	private DefaultComboBoxModel<String> actions = new DefaultComboBoxModel<>(actionsArray);
 	private JComboBox<String> actionCombo = new JComboBox<>(actions);
+	private JTextArea producedCode = new JTextArea(30,30);
 	private JPanel panel = new JPanel();
 	private JButton execBtn = new JButton("Execute");
 	public DemoApp(String title, int width, int height, boolean resizable) {
@@ -47,8 +51,26 @@ public class DemoApp extends ApplicationScreen {
 		setLocationRelativeTo(null);
 		panel.add(actionCombo);
 		panel.add(execBtn);
+		panel.add(producedCode);
 		setContentPane(panel);
+		actionCombo.addActionListener(e->{
+			int index = actionCombo.getSelectedIndex();
+			switch(index){
+				case 0:
+					codeToParse.setText("Message.info(\"This is a simple info dialog with no custom title\");");	
+				break;	
+				case 1:
+					codeToParse.setText("Message.info(\"Message with custom title\",\"Custom title\");");
+				break;
+			}
+			producedCode.setText(codeToParse.toString());
+		});
 		execBtn.addActionListener(e->{
+			int index  = actionCombo.getSelectedIndex();
+			switch(index){
+				case 0:
+					
+			}
 			if(actionCombo.getSelectedIndex()==0)
 				Message.info("This is a simple info dialog with no custom title");
 			else
@@ -67,7 +89,7 @@ public class DemoApp extends ApplicationScreen {
 		// TODO Auto-generated method stub
 
 		SwingUtilities.invokeLater(()->{
-			new DemoApp("Message Demo App",400,400,false).show();
+			new DemoApp("Message Demo App",400,400,false).setVisible(true);
 		});
 	}
 
