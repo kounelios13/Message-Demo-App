c308ad3ae0c7067e60693934785ad0ed3ba7283f
kounelios13 <pkounelios@gmail.com>
2016-12-11 11:50:08 +0200

    added swing layout

--- src/com/demo/DemoApp.java
+++ src/com/demo/DemoApp.java
@@ -5,23 +5,26 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 import com.AppUtils.core.XString;
 import com.AppUtils.gui.ApplicationScreen;
 import com.messages.Message;
+import net.miginfocom.swing.MigLayout;
 
 public class DemoApp extends ApplicationScreen {
 
 
 	private XString codeToParse = new XString();
 	private String[] actionsArray = new String[]{"Show an info message in the current content pane",
-			"Show an info message","Show an info message with custom title"};
+			"Show an info message","Show an info message with custom title","Show a message with custom title in your current window"};
 	private DefaultComboBoxModel<String> actions = new DefaultComboBoxModel<>(actionsArray);
 	private JComboBox<String> actionCombo = new JComboBox<>(actions);
-	private JTextArea producedCode = new JTextArea(30,30);
+	private JTextArea producedCode = new JTextArea(5,34);
 	private JPanel panel = new JPanel();
+	private JScrollPane scrollPane = new JScrollPane();
 	private JButton execBtn = new JButton("Execute");
 	public DemoApp(String title, int width, int height, boolean resizable) {
 		super(title, width, height, resizable);
@@ -50,10 +53,8 @@ public class DemoApp extends ApplicationScreen {
 	protected void initUIElements() {
 		// TODO Auto-generated method stub
 		setLocationRelativeTo(null);
-		panel.add(actionCombo);
-		panel.add(execBtn);
-		panel.add(producedCode);
-		setContentPane(panel);
+		panel.setLayout(new MigLayout("", "[][][]", "[][][]"));
+		panel.add(actionCombo,"cell 0 0");
 		actionCombo.addActionListener(e->{
 			int index = actionCombo.getSelectedIndex();
 			switch(index){
@@ -65,9 +66,13 @@ public class DemoApp extends ApplicationScreen {
 				case 2:
 					codeToParse.setText("Message.info(\"Message with custom title\",\"Custom title\");");
 				break;
+				case 3:
+					codeToParse.setText("Message.info(getContentPane()\", \"This is a message that sticks to your window\", \"Custom title here\");");
+				break;
 			}
 			producedCode.setText(codeToParse.toString());
 		});
+		panel.add(execBtn, "cell 1 0,wrap");
 		execBtn.addActionListener(e->{
 			int index  = actionCombo.getSelectedIndex();
 			switch(index){
@@ -86,6 +91,10 @@ public class DemoApp extends ApplicationScreen {
 					
 			}
 		});
+		panel.add(scrollPane,"flowx,cell 0 2");
+		scrollPane.setViewportView(producedCode);
+		setContentPane(panel);
+		pack();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
@@ -99,7 +108,7 @@ public class DemoApp extends ApplicationScreen {
 		// TODO Auto-generated method stub
 
 		SwingUtilities.invokeLater(()->{
-			new DemoApp("Message Demo App",400,400,false).setVisible(true);
+			new DemoApp("Message Demo App",600,300,false).setVisible(true);
 		});
 	}
 
